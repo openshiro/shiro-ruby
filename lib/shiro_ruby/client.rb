22bcd85d2adcd40a4a388fe50d73269ea6827473
Duncan Miller <dmiller@heatspring.com>
2024-02-24 22:26:12 -0800

    adding gemspec and client class

---
+++ lib/shiro_ruby/client.rb
@@ -0,0 +1,24 @@
+module ShiroRuby
+  class Client
+    def initialize
+      @api_key = ENV["SHIRO_API_KEY"]
+      unless @api_key
+        raise "no api key defined in environment variables"
+      end
+    end
+
+    def parse_url(path)
+      url = URI.parse("https://openshiro.com/api/v1/#{path}")
+
+      request = Net::HTTP::Get.new(url)
+
+      request["Authorization"] = "Bearer #{@api_key}"
+
+      response = Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == 'https') do |http|
+        http.request(request)
+      end
+
+      JSON.parse(response.body)
+    end
+  end
+end
---
+++ lib/shiro_ruby/version.rb
@@ -0,0 +1,3 @@
+module ShiroRuby
+  VERSION = "0.0.1"
+end
